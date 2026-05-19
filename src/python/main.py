c6eaeb22eed9126018b2210360f83310b0fffb27
Israel Alves <alves.israel@aluno.ifce.edu.br>
2026-05-19 09:18:05 -0300

    refactor: modularizar algumas funcionalidades e traduzir para o inglês

--- src/python/main.py
+++ src/python/main.py
@@ -3,74 +3,65 @@ import mediapipe as mp
 import serial
 import time
 
-# --- CONFIGURAÇÃO DA SERIAL ---
-arduino = serial.Serial('/dev/ttyACM0', 9600)  # Troque pela sua porta
-time.sleep(2)  # tempo para inicializar a comunicação
+class FingersTips:
+    FINGER_TIPS_IDS = [4, 8, 12, 16, 20]
 
-# --- CONFIGURAÇÃO DO MEDIAPIPE ---
-camera = cv2.VideoCapture(0)
-mao = mp.solutions.hands
-hands = mao.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.9)
+def get_serial_connection(port : str, baudrate: int):
+    serial_connection = serial.Serial(port, baudrate)
+    time.sleep(2)
+    return serial_connection
+
+cap = cv2.VideoCapture(0)
+hand = mp.solutions.hands
+hands = hand.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.9)
 hand_drawing = mp.solutions.drawing_utils
 
-# IDs das pontas dos dedos
-dedos_ids = [4, 8, 12, 16, 20]
+fingers_tips_ids = FingersTips.FINGER_TIPS_IDS
+
+arduino_connection = get_serial_connection('/dev/ttyACM0', 9600)
 
 while True:
-    _, frame = camera.read()
-    
-    imagem_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    resultados = hands.process(imagem_rgb)
-
-    if resultados.multi_hand_landmarks and resultados.multi_handedness:
-        for juntas, mao_info in zip(resultados.multi_hand_landmarks, resultados.multi_handedness):
-            label = mao_info.classification[0].label  # "Left" ou "Right"
-            hand_drawing.draw_landmarks(frame, juntas, mao.HAND_CONNECTIONS)
-
-            # Pegar coordenadas
-            pontos = []
-            for lm in juntas.landmark:
+    _, frame = cap.read()
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    results = hands.process(rgb_frame)
+    if results.multi_hand_landmarks and results.multi_handedness:
+        for joints, hand_info in zip(results.multi_hand_landmarks, results.multi_handedness):
+            label = hand_info.classification[0].label
+            hand_drawing.draw_landmarks(frame, joints, hand.HAND_CONNECTIONS)
+            points = []
+            for lm in joints.landmark:
                 h, w, _ = frame.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
-                pontos.append((cx, cy))
-
-            dedos_levantados = []
-
-            # --- Lógica para o polegar ---
-            if label == "Right":  # mão direita
-                if pontos[dedos_ids[0]][0] > pontos[dedos_ids[0] - 1][0]:
-                    dedos_levantados.append(0)
+                points.append((cx, cy))
+            raised_fingers = []
+            if label == 'Right':
+                if points[fingers_tips_ids[0]][0] > points[fingers_tips_ids[0] - 1][0]:
+                    raised_fingers.append(0)
                 else:
-                    dedos_levantados.append(1)
-            else:  # mão esquerda
-                if pontos[dedos_ids[0]][0] < pontos[dedos_ids[0] - 1][0]:
-                    dedos_levantados.append(0)
+                    raised_fingers.append(1)
+            else:
+                if points[fingers_tips_ids[0]][0] < points[fingers_tips_ids[0] - 1][0]:
+                    raised_fingers.append(0)
                 else:
-                    dedos_levantados.append(1)
-
-            # --- Lógica para outros dedos ---
+                    raised_fingers.append(1)
             for id in range(1, 5):
-                if pontos[dedos_ids[id]][1] < pontos[dedos_ids[id] - 2][1]:
-                    dedos_levantados.append(1)
+                if points[fingers_tips_ids[id]][1] < points[fingers_tips_ids[id] - 2][1]:
+                    raised_fingers.append(1)
                 else:
-                    dedos_levantados.append(0)
+                    raised_fingers.append(0)
 
-            # Enviar pela serial
-            arduino.write(("".join(map(str, dedos_levantados)) + "\n").encode())
+            arduino_connection.write(("".join(map(str, raised_fingers)) + "\n").encode())
 
-            # Mostrar no vídeo
-            texto = f"{label}: {dedos_levantados}"
-            cv2.putText(frame, texto, (10, 50),
-                        cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            text = f'{label}: {raised_fingers}'
+            cv2.putText(frame, text, (10,50), cv2.FONT_HERSHEY_SIMPLEX, 1, (208, 224, 64), 1)
     else:
-        # Nenhuma mão detectada → envia 00000
-        arduino.write(b"00000\n")
-
-    cv2.imshow("Camera Feed", frame)
+        arduino_connection.write(b"00000\n")
+    cv2.imshow('Hand Detector', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-arduino.close()
-camera.release()
+arduino_connection.close()
+cap.release()
 cv2.destroyAllWindows()
+
